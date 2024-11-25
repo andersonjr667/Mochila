525bb14abd42602a0a5489b1b660a9ffd7a883c3
andersonjr667 <140277030+andersonjr667@users.noreply.github.com>
2024-11-25 10:36:46 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+function comprarAgora() {
+    alert("Obrigado pelo interesse! Redirecionando para o checkout...");
+    // Aqui você pode redirecionar para o processo de compra
+    window.location.href = "https://exemplo.com/comprar";
+}
